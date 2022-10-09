303349f063132d8ff6ef982de329a9fb2121d7d1
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-10-09 17:52:24 +0200

    add - slider component + flat infos

--- src/routes/Flat.tsx
+++ src/routes/Flat.tsx
@@ -2,12 +2,29 @@ import type Flat from "../entities/Flat.interface";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import getFlatData from "../api/getFlatData";
+import styles from "./Flat.module.scss";
+import Carroussel from "../components/Flat/Carroussel";
 
 const FlatPage: React.FunctionComponent = () => {
   const flatData = useLoaderData() as Flat;
   const flatPictures = flatData.pictures;
 
-  return <h2>{flatData.title}</h2>;
+  return (
+    <React.Fragment>
+      <Carroussel pictures={flatPictures} />
+      <section className={styles.infos}>
+        <div className={styles.infos_flat}>
+          <div className={styles.infos_name}>{flatData.title}</div>
+          <div className={styles.infos_location}>{flatData.location}</div>
+          <div className={styles.infos_tags}>
+            {flatData.tags.map((tag) => {
+              return <div className={styles.infos_tag}>{tag}</div>;
+            })}
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
 };
 
 export function loader({ params }: any) {
