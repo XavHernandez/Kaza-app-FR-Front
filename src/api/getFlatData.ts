5e8386896ea2dbb89e3e2c286e7ed144aed54ea6
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-10-10 20:31:52 +0200

    refacto - local json for netlify

--- src/api/getFlatData.ts
+++ src/api/getFlatData.ts
@@ -1,9 +1,11 @@
 import type Flat from "../entities/Flat.interface";
+import json from "./data.json";
 
 async function getFlatData(param: string | undefined) {
-  const response = await fetch("src/api/data.json");
-  if (!response.ok) throw new Error("Une erreur est survenue au chargement");
-  const flatsData: Flat[] = await response.json();
+  // const response = await fetch("src/api/data.json");
+  // if (!response.ok) throw new Error("Une erreur est survenue au chargement");
+  // const flatsData: Flat[] = await response.json();
+  const flatsData: Flat[] = json;
   const flat = flatsData.find((flat) => flat.id === param);
   if (!flat) throw new Error("Aucun id correspondant");
   return flat;
--- src/api/getFlatsData.ts
+++ src/api/getFlatsData.ts
@@ -1,9 +1,11 @@
 import type Flat from "../entities/Flat.interface";
+import json from "./data.json";
 
 async function getFlatsData() {
-  const response = await fetch("src/api/data.json");
-  if (!response.ok) throw new Error("Une erreur est survenue au chargement");
-  const flatsData: Flat[] = await response.json();
+  // const response = await fetch("src/api/data.json");
+  // if (!response.ok) throw new Error("Une erreur est survenue au chargement");
+  // const flatsData: Flat[] = await response.json();
+  const flatsData: Flat[] = json;
   return flatsData;
 }
 
